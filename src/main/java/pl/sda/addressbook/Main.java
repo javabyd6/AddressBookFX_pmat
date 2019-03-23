9e12c2df5b7cf0caaf498a4075ea9906546be9d6
Pawel Matusiak <pawel.matusiak29@gmail.com>
2019-03-23 12:44:41 +0100

    table in window

--- src/main/java/pl/sda/addressbook/Main.java
+++ src/main/java/pl/sda/addressbook/Main.java
@@ -2,24 +2,48 @@ package pl.sda.addressbook;
 
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.sda.addressbook.controller.RootViewController;
+import pl.sda.addressbook.model.Person;
 
 /**
  * @author pmatusiak
  */
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/root.fxml"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    private ObservableList<Person> personList = FXCollections.observableArrayList();
+
+    public Main() {
+        personList.add(new Person("Jan", "Kowalski", "adres", "123", "87-100", "Bdg"));
+        personList.add(new Person("Jan", "Kowalski", "adres", "123", "87-100", "Bdg"));
+    }
+
+    public ObservableList<Person> getPersonList(){
+        return personList;
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/root.fxml"));
+        loader.load();
+
+        Parent root = loader.getRoot();
+        RootViewController rootViewController = loader.getController();
+        rootViewController.setMain(this);
+        rootViewController.loadPerson();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
--- src/main/java/pl/sda/addressbook/controller/RootViewController.java
+++ src/main/java/pl/sda/addressbook/controller/RootViewController.java
@@ -1,16 +1,21 @@
 package pl.sda.addressbook.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import pl.sda.addressbook.Main;
 import pl.sda.addressbook.model.Person;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author pmatusiak
  */
-public class RootViewController {
+public class RootViewController implements Initializable {
 
     @FXML
     private TableView<Person> personTableView;
@@ -40,4 +45,26 @@ public class RootViewController {
     private Button deleteButton;
     @FXML
     private Button saveButton;
+
+    private Main main;
+
+    private Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public void loadPerson(){
+        System.out.println(getMain().getPersonList());
+        personTableView.setItems(getMain().getPersonList());
+        nameCol.setCellValueFactory(c -> c.getValue().nameProperty());
+        lastnameCol.setCellValueFactory(c -> c.getValue().lastnameProperty());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+//        System.out.println(this.main.getPersonList());
+    }
 }
