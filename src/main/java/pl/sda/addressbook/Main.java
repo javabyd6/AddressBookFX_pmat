f79cb8f38fb44aef84661cef991ef857ff7d593d
Pawel Matusiak <pawel.matusiak29@gmail.com>
2019-03-23 09:55:52 +0100

    root scene added

---
+++ src/main/java/pl/sda/addressbook/Main.java
@@ -0,0 +1,20 @@
+package pl.sda.addressbook;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author pmatusiak
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
